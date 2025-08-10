3ec8128c461db54b93f8d006b391070ab6692b8a
Ghanashyam Budhathoki <sdev1252492@gmail.com>
2025-08-10 17:05:58 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+//Basic console.log usage
+//let name = "Samraj" ; 
+//console.log(name);
+let name = "Shyam";
+let age = "12";
+console.log(name,age)
+// To run a Javascript script file, save your code with a .js extension and then execute it either in a browser by linking the file in an HTML page or run it directly using Node.Jjs in the terminal. 
