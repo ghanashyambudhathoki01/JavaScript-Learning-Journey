b369e22db0a053057602a16e7b317900a6a1bde1
Ghanashyam Budhathoki <sdev1252492@gmail.com>
2025-08-11 08:16:07 +0545

    Add files via upload

---
+++ Variable in JS/Variable.js
@@ -0,0 +1,15 @@
+let x = 1;
+let y = 6;
+let z = x + y;
+console.log(z);
+// This is a let variable in js 
+var d = 2;
+var e = 3;
+var f  = 3-2; 
+console.log(f); 
+//This is a var variable in js. 
+const a = 1;
+const b = 2;
+const c = a*b; 
+console.log(c);
+// This is a const variable in js
