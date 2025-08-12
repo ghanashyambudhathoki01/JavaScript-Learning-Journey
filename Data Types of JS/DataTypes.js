6064f035c723e0a91f4388df791165255bcf2f78
Ghanashyam Budhathoki <sdev1252492@gmail.com>
2025-08-12 18:53:00 +0545

    Add files via upload

---
+++ Data Types of JS/DataTypes.js
@@ -0,0 +1,12 @@
+let name = "Shyam"; //string 
+let age = 20; //number 
+let isStudent = true; //boolean 
+let job; //undefined 
+let car = null; //Null 
+let id = Symbol("12")//Symbol 
+
+let person = {name: "Samraj", age : 22}; //object 
+let number = [1,2,3]; // array 
+function greet (){ return "Hello!";}//function 
+let today = new Date (); //Date and object 
+let pattern = /abc/; //regex 
