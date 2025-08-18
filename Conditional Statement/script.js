f67292cf646b692bf2f34a3a3f4fd617248c382a
Ghanashyam Budhathoki <sdev1252492@gmail.com>
2025-08-18 08:36:22 +0545

    Add files via upload

---
+++ Conditional Statement/script.js
@@ -0,0 +1,33 @@
+/*console.log("hello i am learning coditional statement ")
+let age = 35;
+if (age>18){
+    console.log("You can drive") ;
+}
+else {
+    console.log("You cannot drive");
+}*/
+console.log("Hello i am learning condtional statement")
+let age = 98;
+if (age==18){
+    console.log("You can drive")
+}
+else if (age ==0){
+    console.log("You are kidding?")
+}
+else{
+    console.log("You cannnot drive")
+}
+
+a = 6;
+b = 8;
+let c =a>b ? (a-b): (b-a);
+/*
+translates to : 
+if (a>b){
+let  c = a-b;
+}
+else {
+    let c = a-b;
+    }
+    And this is a trenary operator. 
+    */
