fa52f618dc3f8526496c2a487f78faa787322745
Ghanashyam Budhathoki <sdev1252492@gmail.com>
2025-08-21 18:36:24 +0545

    Add files via upload

---
+++ String Js/index.js
@@ -0,0 +1,16 @@
+console.log("This is a strings learning");
+
+let a = "shyam";
+console.log(a[0]); // Accessing the first character of a string.
+console.log(a.length); // Finding the length of a string.
+
+let real_name = "shyam";
+let friend = "harry";
+console.log(`My name is ${real_name} and my friend's name is ${friend}`); // Template literals
+
+let b = "ram";
+console.log(b.toUpperCase()); // Converting string to uppercase
+console.log(b.slice(1, 3)); // Extracting a substring from index 1 to 3
+console.log(b.slice(1));// Extracting substring from index 1 to the end
+console.log(b.replace("ra", "sa")); // Replacing part of the string.
+console.log(b.concat(a)); // Concatenating two strings. 
