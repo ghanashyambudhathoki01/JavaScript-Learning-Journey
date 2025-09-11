65707aaf2d8aff629503e463958b2ff0de8ef944
Ghanashyam Budhathoki <sdev1252492@gmail.com>
2025-09-11 18:01:48 +0545

    Add files via upload

---
+++ Advanced Js/Arrow Function/index.js
@@ -0,0 +1,10 @@
+//Invocation function. 
+function getName(email,age){
+    console.log(arguments)
+}
+getName("email@gmail.com", "ghanashyam");
+
+//Arrow function
+const getName = () => {
+
+}
---
+++ Advanced Js/Function Inside Object/index.js
@@ -0,0 +1,13 @@
+const user = {
+    name: "ghanashyam",
+    age: 20,
+    getLocation: function (){
+        console.log("Location is Nepal");
+        console.log("logging this here =>, this.age");
+
+    },
+    getUrl: function (){
+        return this;
+
+    },
+};
